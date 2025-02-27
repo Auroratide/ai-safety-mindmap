5f324b59fd0f71367eb34c6db2cd5ccf4f0e213f
Auroratide <tf.auroratide@gmail.com>
2025-02-27 07:50:15 -0500

    Prevent immediately closing dialogs on mobile when opening them

--- src/dialog/hide.ts
+++ src/dialog/hide.ts
@@ -1,14 +1,25 @@
 export function hideDialogOnBackdrop() {
 	const dialog = document.querySelector<HTMLDialogElement>("#dialog")
+	if (dialog == null) return
 
-	if (dialog != null) {
-		dialog.addEventListener("click", (e: MouseEvent) => {
-			const bounds = dialog.getBoundingClientRect()
-			const isInDialog = (bounds.top <= e.clientY && e.clientY <= bounds.top + bounds.height && bounds.left <= e.clientX && e.clientX <= bounds.left + bounds.width)
+	const handleBackdropClick = (e: MouseEvent) => {
+		const bounds = dialog.getBoundingClientRect()
+		const isInDialog = (bounds.top <= e.clientY && e.clientY <= bounds.top + bounds.height && bounds.left <= e.clientX && e.clientX <= bounds.left + bounds.width)
 
-			if (!isInDialog) {
-				dialog.close()
-			}
-		})
+		if (!isInDialog) {
+			dialog.close()
+		}
 	}
+
+	dialog.addEventListener("toggle", (e: ToggleEvent) => {
+		if (e.newState === "open") {
+			// The timeout prevents a click from immediately happening as the dialog is opened
+			// This seems to happen on mobile browsers
+			setTimeout(() => {
+				dialog.addEventListener("click", handleBackdropClick)
+			}, 250)
+		} else {
+			dialog.removeEventListener("click", handleBackdropClick)
+		}
+	})
 }
